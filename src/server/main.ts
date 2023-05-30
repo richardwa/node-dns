e65073c4b2231cafe8f1e661978de0670ca839a1
Richard Wang <rwang83@gmail.com>
2023-05-30 01:46:18 -0400

    update

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,1 +1,79 @@
-import './http'
+import http from 'http'
+import { serveStaticFile } from './fileserver'
+import { endPoints } from '@/common/config'
+// @ts-ignore
+import { dnsServer } from './dns'
+import { getHosts } from './hosts'
+import type { BlockList, TimerStop } from '@/common/types'
+
+const args = process.argv.slice(2)
+const port = args[0]
+const blockList: BlockList = {}
+const timers: { [s: string]: NodeJS.Timeout } = {}
+const timerStop: TimerStop = {}
+
+dnsServer(blockList)
+
+const server = http.createServer((req, res) => {
+  let pattern = endPoints.state
+  if (req.url?.startsWith(pattern)) {
+    getHosts().then((hosts) => {
+      res.writeHead(200, { 'Content-Type': 'text/plain' })
+      res.end(
+        JSON.stringify({
+          blockList,
+          hosts,
+          timerStop
+        })
+      )
+    })
+    return
+  }
+
+  pattern = endPoints.block
+  if (req.url?.startsWith(pattern)) {
+    const query = req.url.substring(pattern.length + 1)
+    const params = new URLSearchParams(query)
+    const ip = params.get('ip')
+    if (ip) {
+      clearTimeout(timers[ip])
+      delete timerStop[ip]
+      blockList[ip] = ['']
+      res.writeHead(200)
+      res.end()
+    }
+    return
+  }
+
+  pattern = endPoints.unblock
+  if (req.url?.startsWith(pattern)) {
+    const query = req.url.substring(pattern.length + 1)
+    const params = new URLSearchParams(query)
+    const ip = params.get('ip') || ''
+    const duration = parseInt(params.get('duration') || '0')
+    clearTimeout(timers[ip])
+    delete timerStop[ip]
+    delete blockList[ip]
+
+    if (duration) {
+      const durationMillis = duration * 60 * 1000
+      timerStop[ip] = Date.now() + durationMillis
+      timers[ip] = setTimeout(() => {
+        blockList[ip] = ['']
+        delete timerStop[ip]
+      }, durationMillis)
+    } else {
+      delete timerStop[ip]
+    }
+    res.writeHead(200)
+    res.end()
+    return
+  }
+
+  serveStaticFile(req, res)
+  return
+})
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
+})
